c5102663359f30514122a26d9ce1f7fa663daa84
lolleko <lolleko@users.noreply.github.com>
2018-01-25 14:52:23 +0100

    Added some Lua base functions

--- declarations/server/dota-std.d.ts
+++ declarations/server/dota-std.d.ts
@@ -1,2 +1,24 @@
 declare function print(...messages: any[]): void;
 declare function require(module: string): any;
+
+declare function type(variable: any): string;
+
+declare function setmetatable(tbl: table, metaTbl: table)
+declare function getmetatable(tbl: table): table
+
+declare function assert(v: any, message?: string): any
+declare function error(message: string, level?: number)
+
+declare function dofile(filename?: string): any
+
+declare function getfenv(f: Function): table
+declare function setfenv(f: Function, tbl : table) : Function
+
+declare function load(func: Function, chunkname?: string): Function
+declare function loadfile(filename?: string): Function
+declare function loadstring(str: string, chunkname?: string): Function
+
+declare function pcall(f, ...args: any[]) : any
+
+declare function tonumber(e:any, base?: number) : number
+declare function tostring(e : any) : string
