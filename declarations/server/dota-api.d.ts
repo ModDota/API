76236a303a52fb80a0207f122f3ccb9ec31b4c89
Zapp Brannigan <zapp.brannigan.dota@gmail.com>
2018-02-27 20:57:52 +0100

    Move client side Lua types to separate file (#24)
    * Move client side Lua types to separate file
    
    * Update dota-api.d.ts

--- declarations/server/dota-api.d.ts
+++ declarations/server/dota-api.d.ts
@@ -5989,13 +5989,3 @@ declare abstract class CScriptHTTPRequest {
      */
     SetHTTPRequestRawPostBody(arg1: string, arg2: string): boolean;
 }
-
-// Client side Lua types
-type C_BaseEntity = CBaseEntity;
-type C_BaseModelEntity = CBaseModelEntity;
-type C_DOTABaseAbility = CDOTABaseAbility;
-type C_DOTA_Ability_Lua = CDOTA_Ability_Lua;
-type C_DOTA_BaseNPC = CDOTA_BaseNPC;
-type C_DOTA_Item = CDOTA_Item;
-type C_DOTA_Item_Lua = CDOTA_Item_Lua;
-type C_DOTA_Modifier_Lua = CDOTA_Modifier_Lua;
